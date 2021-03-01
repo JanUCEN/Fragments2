50b9cda50203790f54f46cb920ffa10081552988
= <=>
2021-03-01 11:56:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/fragments2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.fragments2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
