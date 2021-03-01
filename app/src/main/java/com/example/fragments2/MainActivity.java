93fc5a09ff3053b07fbd1bf333eed7075c3d2cb0
= <=>
2021-03-01 12:13:52 +0000

    Create sample data

--- app/src/main/java/com/example/fragments2/MainActivity.java
+++ app/src/main/java/com/example/fragments2/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.lifecycle.MutableLiveData;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public Bundle sampleData() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Ciaran Clifford");
+        list.add("Benny Bryant");
+        list.add("Georgie Burt");
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList("names",list);
+        return bundle;
+    }
 
 }
