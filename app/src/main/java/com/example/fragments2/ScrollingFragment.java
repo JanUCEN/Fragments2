ea1544327741ac2e71e4da6832c2ecb3c4b97b9c
= <=>
2021-03-01 12:29:51 +0000

    Add bundle contents to Fragment view

--- app/src/main/java/com/example/fragments2/ScrollingFragment.java
+++ app/src/main/java/com/example/fragments2/ScrollingFragment.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ScrollingFragment extends Fragment {
 
@@ -16,6 +23,22 @@ public class ScrollingFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_scrolling, container, false);
+        View view = inflater.inflate(R.layout.fragment_scrolling, container, false);
+
+
+        ArrayList users = getArguments().getStringArrayList("names");
+        Iterator it = users.iterator();
+
+
+        LinearLayout layout = view.findViewById(R.id.linearScrollingLayout);
+
+        while(it.hasNext()) {
+            TextView text = new TextView(getContext());
+            text.setText(it.next().toString());
+
+            layout.addView(text);
+        }
+
+        return view;
     }
 }
